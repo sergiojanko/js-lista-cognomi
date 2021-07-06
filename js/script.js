8fc934ec2c816d95133db0d2c411054fd93b64eb
sergiojanko <janko.sergio1998@gmail.com>
2021-07-06 16:39:31 +0200

    capitalize-strings

--- js/script.js
+++ js/script.js
@@ -13,17 +13,23 @@ console.log(userLastName);
 console.table(surnames);
 surnames.push(userLastName);
 
-// Sort Array
-
-surnames.sort();
-console.table(surnames);
 
 var lastNameListElement = document.getElementById("lastnames-list");
 var listItems = "";
 
 for (var i = 0; i < surnames.length; i++) {
+    surnames[i] = surnames[i][0].toUpperCase() + surnames[i].slice(1);
+}
+
+// Sort Array
+
+surnames.sort();
+console.table(surnames);
+
+for (var i = 0; i < surnames.length; i++) {
     listItems += "<li>" + (i + 1) + " " + surnames[i] + "</li>"
     console.log((i + 1) + " " + surnames[i])
 }
+
 
 lastNameListElement.innerHTML = listItems;
