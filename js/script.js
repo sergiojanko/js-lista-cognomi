3c51ef0446fa82c210b3e76c7a82174fecc55e19
sergiojanko <janko.sergio1998@gmail.com>
2021-07-06 15:30:29 +0200

    output

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 var surnames = ["Bianchi", "Neri", "Rossi", "Verdi", "Gialli"]
 var userLastName;
 
+
+// Collect user data
+
 do {
     userLastName = prompt("Inserisci il tuo cognome");
 } while (!userLastName);
@@ -9,5 +12,18 @@ console.log(userLastName);
 
 console.table(surnames);
 surnames.push(userLastName);
+
+// Sort Array
+
 surnames.sort();
 console.table(surnames);
+
+var lastNameListElement = document.getElementById("lastnames-list");
+var listItems = "";
+
+for (var i = 0; i < surnames.length; i++) {
+    listItems += "<li>" + (i + 1) + " " + surnames[i] + "</li>"
+    console.log((i + 1) + " " + surnames[i])
+}
+
+lastNameListElement.innerHTML = listItems;
