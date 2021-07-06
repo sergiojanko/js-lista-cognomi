b5bc34af6a5c5262c651a6cdcea080249a13ef94
sergiojanko <janko.sergio1998@gmail.com>
2021-07-06 17:20:16 +0200

    fixed-string-capitalize

--- js/script.js
+++ js/script.js
@@ -8,6 +8,8 @@ do {
     userLastName = prompt("Inserisci il tuo cognome");
 } while (!userLastName);
 
+userLastName = userLastName[0].toUpperCase() + userLastName.slice(1);
+
 console.log(userLastName);
 
 console.table(surnames);
@@ -17,9 +19,6 @@ surnames.push(userLastName);
 var lastNameListElement = document.getElementById("lastnames-list");
 var listItems = "";
 
-for (var i = 0; i < surnames.length; i++) {
-    surnames[i] = surnames[i][0].toUpperCase() + surnames[i].slice(1);
-}
 
 // Sort Array
 
@@ -27,8 +26,8 @@ surnames.sort();
 console.table(surnames);
 
 for (var i = 0; i < surnames.length; i++) {
-    listItems += "<li>" + (i + 1) + " " + surnames[i] + "</li>"
-    console.log((i + 1) + " " + surnames[i])
+    listItems += "<li>" + (i + 1) + " " + surnames[i] + "</li>";
+    console.log((i + 1) + " " + surnames[i]);
 }
 
 
