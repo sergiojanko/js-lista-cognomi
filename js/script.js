e0651b747ac26c48f21460b69f3accd22a100ba6
sergiojanko <janko.sergio1998@gmail.com>
2021-07-06 17:50:43 +0200

    add-position-index-output

--- js/script.js
+++ js/script.js
@@ -16,19 +16,33 @@ console.table(surnames);
 surnames.push(userLastName);
 
 
-var lastNameListElement = document.getElementById("lastnames-list");
-var listItems = "";
-
-
 // Sort Array
 
 surnames.sort();
 console.table(surnames);
 
+
+var lastNameListElement = document.getElementById("lastnames-list");
+var listItems = "";
+
+var lastNamePosition = -1;
+
 for (var i = 0; i < surnames.length; i++) {
     listItems += "<li>" + (i + 1) + " " + surnames[i] + "</li>";
     console.log((i + 1) + " " + surnames[i]);
+    if (surnames[i] === userLastName) {
+        lastNamePosition = i + 1;
+        console.log(lastNamePosition);
+    }
+}
+
+lastNameListElement.innerHTML = listItems;
+
+var positionElement = document.getElementById("output-position");
+
+if (lastNamePosition >= 0) {
+    console.log("Il tuo cognome si trova in ", lastNamePosition , "° posizione");
+    positionElement.innerHTML = "Il tuo cognome si trova in " + lastNamePosition + "° posizione";
 }
 
 
-lastNameListElement.innerHTML = listItems;
