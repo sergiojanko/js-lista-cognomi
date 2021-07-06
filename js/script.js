306015f379c597af0f2fb2fd99bc716b580b96bc
sergiojanko <janko.sergio1998@gmail.com>
2021-07-06 15:20:22 +0200

    sort-array

--- js/script.js
+++ js/script.js
@@ -2,6 +2,12 @@ var surnames = ["Bianchi", "Neri", "Rossi", "Verdi", "Gialli"]
 var userLastName;
 
 do {
-    userLastName = prompt("Inserisci il tuo cognome")
+    userLastName = prompt("Inserisci il tuo cognome");
 } while (!userLastName);
 
+console.log(userLastName);
+
+console.table(surnames);
+surnames.push(userLastName);
+surnames.sort();
+console.table(surnames);
